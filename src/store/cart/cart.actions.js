0b24ff2fb3396f2e222d551748a7320b93f6a8c7
psnarkhede <piyushnarkhede02@gmail.com>
2022-06-13 17:17:44 +0530

    E4

--- src/store/cart/cart.actions.js
+++ src/store/cart/cart.actions.js
@@ -6,20 +6,4 @@ import { ADD_ITEM_TO_CART_LOADING } from "./cart.types"
 
 export const addtocartapi = (dispatch,id) => {
     dispatch({type:ADD_ITEM_TO_CART_LOADING})
-
-    axios
-    .get(`http://localhost:8080/cartItems/${id}`)
-    .then((res) => console.log(res.data))
-    .catch(() => 
-
-    axios
-    .post("http://localhost:8080/cartItems",{
-        "productId": id,
-      "count": 1,
-    }).then((res) => console.log(res.data))
-    );
-
-   /* axios.patch(`http://localhost:8080/cartItems/${id}`,{count:res.data.count+1})
-    .then((res) => dispatch({type:ADD_ITEM_TO_CART_LOADING}))
-    )*/
 }
